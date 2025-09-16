c2aa5f660893e2250f7648c8924e086412549a61
trevorhowat <tah5950@psu.edu>
2025-09-15 20:22:38 -0700

    13-Sets-Up-Spring-Boot-Project

---
+++ backend/src/main/java/psu/basepaths/BasepathsApplication.java
@@ -0,0 +1,13 @@
+package psu.basepaths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasepathsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasepathsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/psu/basepaths/BasepathsApplicationTests.java
@@ -0,0 +1,13 @@
+package psu.basepaths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasepathsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
