8b274dc322a01418864c91002e4db685ed7234f1
trevorhowat <tah5950@psu.edu>
2025-10-04 19:13:14 -0700

    20-fix tests

--- frontend/src/pages/CreateAccount.jsx
+++ frontend/src/pages/CreateAccount.jsx
@@ -8,8 +8,7 @@ import {
   Alert,
 } from "@mui/material";
 import { saveToken } from "../utils/authUtils";
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+import { API_BASE_URL } from '../config';
 
 function CreateAccount() {
   const [username, setUsername] = useState("");
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,8 +9,7 @@ import {
   Alert,
 } from "@mui/material";
 import { saveToken } from "../utils/authUtils";
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+import { API_BASE_URL } from '../config';
 
 function Login() {
   const [username, setUsername] = useState("");
