45175e66d078bd544005dcbc157c01f082c1afaa
trevorhowat <tah5950@psu.edu>
2025-11-03 19:47:23 -0800

    82-Create Graph Objects

---
+++ backend/src/main/java/psu/basepaths/utilities/graphObjects/Node.java
@@ -0,0 +1,12 @@
+package psu.basepaths.utilities.graphObjects;
+
+import java.util.Date;
+
+import psu.basepaths.model.BallparkDTO;
+
+public class Node {
+    public Date date;
+    public double latitude;
+    public double longitude;
+    public BallparkDTO ballpark;
+}
---
+++ backend/src/main/java/psu/basepaths/utilities/graphObjects/Path.java
@@ -0,0 +1,9 @@
+package psu.basepaths.utilities.graphObjects;
+
+import java.util.List;
+
+public class Path {
+    public List<Node> path;
+    public int totalGames;
+    public int uniqueBallparks;
+}
